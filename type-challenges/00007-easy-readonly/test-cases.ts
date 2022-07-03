3eeb152393a504ef0416b868738eade8b363a526
violetrosez <violetrosezq@gmail.com>
2022-07-03 10:47:38 +0800

    init

---
+++ type-challenges/00007-easy-readonly/test-cases.ts
@@ -0,0 +1,15 @@
+// you should make this work
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
+]
+
+interface Todo1 {
+  title: string
+  description: string
+  completed: boolean
+  meta: {
+    author: string
+  }
+}
